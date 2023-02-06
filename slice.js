36ef1a9bb2f1f6542713e0c3ed52811e82d4654e
islamajabul <ajabulmd1@gmail.com>
2023-02-06 18:50:21 +0800

    slice & splice

---
+++ slice.js
@@ -0,0 +1,4 @@
+const friends = [12,45,58,68,25,12,99,62];
+
+const partial = friends.slice(2,5);
+console.log(partial)
---
+++ splice.js
@@ -0,0 +1,5 @@
+const friends = [12,45,58,68,25,12,99,62];
+console.log(friends)
+const partial = friends.splice(3,2,100,200,300);
+console.log(partial)
+console.log(friends);
